66af285e50170736e13636ee89235a821a36f9f2
taga3s <bepozurapu274@gmail.com>
2026-06-07 21:39:56 +0900

    Add nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,51 @@
+-- share clipboard with OS
+vim.opt.clipboard:append('unnamedplus,unnamed')
+
+-- use 2-spaces indent
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = e
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- scroll offset as 3 lines
+vim.opt.scrolloff = 3
+
+-- move the cursot to the previous/next line across the first/last charactor
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+
+vim.api.nvim_create_user_command(
+  'InitLua',
+  function()
+    vim.cmd.edit(vim.fn.stdpath('config') .. '/init.lua')
+  end,
+  { desc = 'Open init.lua' }
+)
+
+-- augroup for this config file
+local augroup = vim.api.nvim_create_augroup('init.lua', {})
+
+-- wrapper function to use internal augroup
+local function create_autocmd(event, opts)
+  vim.api.nvim_create_autocmd(event, vim.tbl_extend('force', {
+    group = augroup,
+  }, opts))
+end
+
+-- https://vim-jp.org/vim-users-jp/2011/02/20/Hack-202.html
+create_autocmd('BufWritePre', {
+        pattern = '*',
+        callback = function(event)
+                local dir = vim.fs.dirname(event.file)
+                local force = vim.v.cmdbang == 1
+                if vim.fn.isdirectory(dir) == 0
+                        and (force or vim.fn.confirm('"' .. dir .. '" does not exist. Create?', "&Yes\n&No") == 1) then
+                  vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(), vim.opt.termencoding:get()), 'p')
+                end
+        end,
+        desc = 'Auto mkdir to save file'
+})
+
+vim.keymap.set('n', 'p', 'p`]', { desc = 'Paste and move to the end' })
+vim.keymap.set('n', 'P', 'P`]', { desc = 'Paste and move to the end' }) 
+
