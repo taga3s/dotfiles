f3b816551607bb7869b2ec6e626024929aa66fe5
taga3s <bepozurapu274@gmail.com>
2026-06-27 23:00:15 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,8 @@ function link_config() {
 
 CONFIG_PATH="${CONFIG_PATH:-$HOME/.config}"
 
+link_config homebrew/Brewfile "$HOME/.homebrew"
 link_config wezterm/wezterm.lua "$CONFIG_PATH/wezterm"
 link_config starship/starship.toml "$CONFIG_PATH"
 link_config nvim "$CONFIG_PATH/nvim"
+
