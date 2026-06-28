45e080ad092bc7c0eed385bb9d85310a7e32706d
taga3s <107479598+taga3s@users.noreply.github.com>
2026-06-28 20:54:57 +0900

    Add gh/config.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,5 +15,4 @@ link_config starship/starship.toml "$XDG_CONFIG_HOME"
 link_config nvim/init.lua "$XDG_CONFIG_HOME/nvim"
 link_config git/ignore "$XDG_CONFIG_HOME/git"
 link_config git/config "$XDG_CONFIG_HOME/git"
-
-
+link_config gh/config.yml "$XDG_CONFIG_HOME/gh"
