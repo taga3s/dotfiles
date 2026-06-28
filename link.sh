65a7399b28f66bb6d5c4cff8a1f06c614648dc80
taga3s <107479598+taga3s@users.noreply.github.com>
2026-06-28 21:57:32 +0900

    Update link.sh

--- link.sh
+++ link.sh
@@ -9,10 +9,10 @@ function link_config() {
 
 XDG_CONFIG_HOME="${CONFIG_PATH:-$HOME/.config}"
 
-link_config homebrew/Brewfile "$HOME/.homebrew"
-link_config wezterm/wezterm.lua "$XDG_CONFIG_HOME/wezterm"
+link_config homebrew/Brewfile "$HOME/.homebrew/Brewfile"
+link_config wezterm/wezterm.lua "$XDG_CONFIG_HOME/wezterm/wezterm.lua"
 link_config starship/starship.toml "$XDG_CONFIG_HOME"
-link_config nvim/init.lua "$XDG_CONFIG_HOME/nvim"
-link_config git/ignore "$XDG_CONFIG_HOME/git"
-link_config git/config "$XDG_CONFIG_HOME/git"
-link_config gh/config.yml "$XDG_CONFIG_HOME/gh"
+link_config nvim/init.lua "$XDG_CONFIG_HOME/nvim/init.lua"
+link_config git/ignore "$XDG_CONFIG_HOME/git/ignore"
+link_config git/config "$XDG_CONFIG_HOME/git/config"
+link_config gh/config.yml "$XDG_CONFIG_HOME/gh/config.yml"
