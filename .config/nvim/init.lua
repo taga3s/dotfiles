60489a94e1f2b95550847c53330dad4a8e667af6
taga3s <107479598+taga3s@users.noreply.github.com>
2026-07-29 23:11:07 +0900

    Update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,5 @@
 require("config.lazy")
 
--- disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 vim.opt.termguicolors = true
 
 -- share clipboard with OS
---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,24 @@
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    },
+  },
+}
