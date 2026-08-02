b90f79427c544d5dad0d4d0fec4500b05f7fca93
taga3s <107479598+taga3s@users.noreply.github.com>
2026-08-02 16:26:34 +0900

    Update nvim

---
+++ .config/nvim/lua/plugins/barbar.lua
@@ -0,0 +1,15 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  init = function() vim.g.barbar_auto_setup = false end,
+  opts = {
+      animation = true,
+      auto_hide = true,
+      clickable = true,
+      insert_at_end = true,
+  },
+}
+
